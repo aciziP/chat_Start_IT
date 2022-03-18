9beef84c6aef94065dc4b7c06777ee72e1ddeaaf
aciziP <65186252+aciziP@users.noreply.github.com>
2022-03-18 12:06:53 +0200

    viss

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let manaZina = document.querySelector('.manaZina');//Jāraksta nosaukums cs stilā
+//reference atsauce uz svaigāko ziņu (jo bez .value)
+let chataZinas = document.querySelector('.chataZinas')
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+
+    chataZinas.innerHTML+='<br/>'+manaZina.value
+}
+
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('chatazinas.txt')
+    let dati = await datiNoServera.text()
+    //console.log(dati)
+    chataZinas.innerHTML = dati
+}
+
+setInterval(ieladetChataZinas,1000)
