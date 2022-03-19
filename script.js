7be5056073be4a5ca206598c719c555b2d8f0444
aciziP <65186252+aciziP@users.noreply.github.com>
2022-03-19 10:32:03 +0200

    lasa vardu

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const API = "https://chat.viktorijapizica.repl.co"
 let manaZina = document.querySelector('.manaZina');//Jāraksta nosaukums cs stilā
 //reference atsauce uz svaigāko ziņu (jo bez .value)
 let chataZinas = document.querySelector('.chataZinas')
+let vards = document.querySelector('.vards')
 
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
-    chataZinas.innerHTML+='<br/>'+manaZina.value
+    //chataZinas.innerHTML+='<br/>'+manaZina.value
+
+    fetch(API+'/sutit/'+vards.value+'/'+manaZina.value)
 }
 
 async function ieladetChataZinas()
@@ -19,4 +22,30 @@ async function ieladetChataZinas()
     chataZinas.innerHTML = dati
 }
 
-setInterval(ieladetChataZinas,1000)
+//setInterval(ieladetChataZinas,1000)
+
+async function ieladetChataZinasJson()
+{
+    let datiNoServera = await fetch(API + '/lasit');
+    let dati = await datiNoServera.json();
+    
+    //console.log(await dati[0]['zina'] )
+    chataZinas.innerHTML = '';
+    
+    i = 0;
+    while ( i < await dati.length )
+    {
+        //console.log(i);
+        let laiks = '[<i>' + '????          ' + '</i>] ';
+        if ("laiks" in dati[i]) {
+            laiks = '[<i>' + dati[i]['laiks'] + '</i>] ';
+        }
+        chataZinas.innerHTML = chataZinas.innerHTML + laiks + dati[i]['vards']+': '+dati[i]['zina']+'<br />';
+
+        i = i+1;
+    }
+
+    chataZinas.scrollTop = chataZinas.scrollHeight;
+}//beidzas ieladetChataZinasJson()
+
+setInterval( ieladetChataZinasJson, 1000 )
